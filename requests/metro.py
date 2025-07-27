070d32b0d5817d1e1917c99f347885a5377dffe9
Mr.Puls <impulsar007@gmail.com>
2025-07-27 20:54:16 +0300

    feat: converted to API with soup for atb

--- requests/metro.py
+++ requests/metro.py
@@ -1,4 +1,9 @@
+import csv
+from datetime import datetime
+
 import requests
+
+time = datetime.now()
 
 headers = {
     'Host': 'stores-api.zakaz.ua',
@@ -20,16 +25,52 @@ headers = {
     'content-language': 'uk',
 }
 
-category_url = 'https://stores-api.zakaz.ua/stores/48215614/categories/'
-products_url = 'https://stores-api.zakaz.ua/stores/48215614/categories/dairy-and-eggs-metro/products?page=1'
-categories = []
+def get_categories():
+    category_params = {
+        'only_parents': 'false',
+    }
+    category_url = 'https://stores-api.zakaz.ua/stores/48215614/categories/'
+    category_response = requests.get(category_url, headers=headers, params=category_params)
 
-params = {
-    'only_parents': 'false',
-}
+    return [(x['id'], x['title']) for x in category_response.json()]
 
-response = requests.get(category_url, headers=headers, params=params)
+def get_products(category: tuple):
+    products = []
+    products_url = f'https://stores-api.zakaz.ua/stores/48215614/categories/{category[0]}/products'
+    product_params_page = 1
+    while True:
+        product_params = {
+            'page': f'{product_params_page}'
+        }
+        products_response = requests.get(products_url, headers=headers, params=product_params)
+        products_response_results = products_response.json()['results']
 
-for item in response.json():
-    categories.append((item['id'], item['title']))
+        if not products_response_results:
+            break
 
+        for product in products_response_results:
+            products.append({
+                'name': product['title'],
+                'price': format(product['price'] / 100, '.2f') + ' грн',
+                'ref': product['web_url'],
+                'category': category[1],
+            })
+
+        product_params_page += 1
+
+    if len(products) == products_response.json()['count']:
+        print(f'All items collected!')
+    return products
+
+
+with open('metro.csv', 'w', newline='', encoding="utf-8") as csvfile:
+    fieldnames = ['name', 'price', 'ref', 'category']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+    categories = get_categories()
+    for ctg in categories:
+        print(f"Processing: {ctg[1]}")
+        writer.writerows(get_products(ctg))
+
+print(f"All completed!\nElapsed time: {datetime.now() - time}")
--- requests/varus.py
+++ requests/varus.py
@@ -13,11 +13,6 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:140.0) Gecko/20100101 Firefox/140.0"
 }
 
-
-
-
-
-
 def get_product_categories():
     categories_request_data = {
         "_availableFilters": [],
